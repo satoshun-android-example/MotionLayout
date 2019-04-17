9fcd4125450d842e97edbbc1420dae761d87a8de
SatoShun <shun.sato1@gmail.com>
2019-04-17 08:10:09 +0000

    scene5

---
+++ app/src/main/java/com/github/satoshun/example/MainActivity4.kt
@@ -0,0 +1,54 @@
+package com.github.satoshun.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.doOnLayout
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.main_act4.*
+
+class MainActivity4 : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act4)
+
+    Glide.with(this)
+      .load("https://pbs.twimg.com/profile_images/1108958487801786368/wwk-3ysJ_400x400.jpg")
+      .into(left)
+
+    Glide.with(this)
+      .load("https://pbs.twimg.com/profile_images/1108958487801786368/wwk-3ysJ_400x400.jpg")
+      .into(right)
+
+    root.doOnLayout {
+      root.setTransitionListener(object : MotionLayout.TransitionListener {
+        private var state: Int = 0
+
+        override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+        }
+
+        override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+        }
+
+        override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+        }
+
+        override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+          if (state == 0) {
+            root.setTransition(R.id.scene5_rotation_begin, R.id.scene5_rotation_end)
+            root.setTransitionDuration(1000)
+            root.transitionToEnd()
+          }
+          if (state == 1) {
+            root.setTransition(R.id.scene5_icon_begin, R.id.scene5_icon_end)
+            root.setTransitionDuration(1000)
+            root.transitionToEnd()
+          }
+          state++
+        }
+      })
+
+      root.transitionToEnd()
+    }
+  }
+}
