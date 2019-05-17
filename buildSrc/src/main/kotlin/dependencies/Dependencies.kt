a5d8cd104717643ae16ac3226a0c967fc0b3fc18
SatoShun <shun.sato1@gmail.com>
2019-05-17 03:05:51 +0000

    updatet

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -7,7 +7,7 @@ const val TARGET_SDK = 28
 private const val VKOTLIN = "1.3.31"
 private const val VNAVIGATION = "1.0.0"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-alpha13"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-beta01"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val SAFEARGS_PLUGIN = "android.arch.navigation:navigation-safe-args-gradle-plugin:$VNAVIGATION"
 const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
@@ -23,7 +23,7 @@ const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0-alpha06"
 
 const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0-alpha04"
 const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.1.0-alpha02"
-const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-alpha5"
+const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-beta1"
 const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
 
 const val CONTENTPAGER = "androidx.contentpager:contentpager:1.0.0-rc01"
