8cb46736c9d8096d14cba7f33faf7c13f2796ac8
SatoShun <shun.sato1@gmail.com>
2019-05-28 11:31:52 +0000

    hoge

--- app/src/main/java/com/github/satoshun/example/RecyclerViewFlowExample.kt
+++ app/src/main/java/com/github/satoshun/example/RecyclerViewFlowExample.kt
@@ -1,20 +1,21 @@
 package com.github.satoshun.example
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.recycler_act.*
-
-private val labels = listOf("1", "a", "bb", "gaag", "gaglgg", "gmlmagna", "gagaga")
 
 class RecyclerViewFlowExample : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.recycler_act)
 
-    labels.forEach {
+    repeat(100) {
       val chip = Chip(this)
-      chip.text = it
+      chip.text = it.toString()
+
+      chip.id = View.generateViewId()
 
       motion.addView(chip)
       flow.addView(chip)
